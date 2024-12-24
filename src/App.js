a625bb8592fd34383557db15bccf16d9a1d59f2e
ShubhamYadav7106 <shubhamyadav98372@gmail.com>
2024-12-24 10:13:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './Components/header.js';
+import Notes from './Components/notes.js';
+
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <Notes/>
+    </div>
+  );
+}
+
+export default App;
